e00f8aef1954f49f79e57292fd575efda4b4c7cc
SOHAM CHATTERJEE <101916371+phoe-nix99@users.noreply.github.com>
2024-11-18 19:08:55 +0530

    New Code

---
+++ Ascii.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Ascii {
+     char b[] = new char[10];
+     static int f = 0;
+              public static void main(String[] args) {
+                  @SuppressWarnings("resource")
+                 Scanner sc = new Scanner(System.in);
+                  System.out.println("Enter the String: ");
+                  String a = sc.nextLine();
+                  char [] b = a.toCharArray();
+                  for (int i = 0;i<b.length;i++){
+     
+                     char c = b[i];
+                     int assci = c;
+                     f += assci;
+
+                System.out.println("The element at index "+i+" is:"+b[i]);
+                System.out.println("The ASCII value at index "+i+" is:"+assci);
+               
+             }
+             System.out.println("The sum of the letters are: "+f);
+
+    }
+    
+}
